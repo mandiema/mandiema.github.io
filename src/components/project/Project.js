f7a14d14ce1e1ca21c450faca8214fcd4e778223
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-14 20:06:44 -0800

    add project component

---
+++ src/components/project/Project.js
@@ -0,0 +1,12 @@
+export default function Project({image, title, description}) {
+    return (
+        <div>
+            <img src={image} alt="" height="200vh" width="350vh"></img>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <button>See Project</button>
+        </div>
+    )
+}
+
+// goal: create project grid 
