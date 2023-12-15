19d96984272183d0e2ef79eb78f33e15be7e9cb0
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-14 18:04:49 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import headshot from './headshot.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={headshot} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This girl is working on her site. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
