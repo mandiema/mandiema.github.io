cf38c17eed829c556662cb6d96f98af96df2d530
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-14 20:07:22 -0800

    project styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import headshot from './headshot.jpg';
+
+import Project from './components/project/Project.js';
+import img from './components/project/img/wc.jpg';
+
 import './App.css';
+
 
 function App() {
   return (
@@ -8,9 +13,14 @@ function App() {
         <h2>Amanda Ma</h2>
         <img src={headshot} className="App-logo" alt="logo" />
         <p>
-          This girl is working on her site. 
+          This girl is working on her site!
         </p>
       </header>
+      <section className='Project'>
+      <Project {...{image:img, 
+        title:"Latent Dirichlet Allocation", 
+        description:"Fall'19 Data Consulting Deliverable, SAAS"}}/>
+      </section>
     </div>
   );
 }
