adf588d2daa3397dac231ad4b6dcc27c74da4a5a
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-15 12:00:22 -0800

    styling fix

--- src/components/project/ProjectGrid.js
+++ src/components/project/ProjectGrid.js
@@ -4,6 +4,7 @@ import Project from './Project.js';
 export default function ProjectGrid() {
     return (
         <section class=".ProjectGrid">
+            <h1>Projects</h1>
             <Project {...PROJECTS[0]} />
             <Project {...PROJECTS[1]} />
         </section>
