26eaaf0c6ee837b0ebb467ba2a0cbf1bdc7d7eca
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-15 16:44:37 -0800

    styling fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,36 @@
 import headshot from './headshot.jpg';
+import email from './email.png';
+import arrow from './arrow.png';
 
 export default function Header() {
     return (
         <div className="profile-header">
             <h1>Amanda Ma</h1>
             <img src={headshot} className="headshot" alt="logo" />
-            <h3>
-            UC Berkeley, BA Data Science '21
-            </h3>
-            <h4>
-            Former Data Engineer @ Meta, 2022-23
-            </h4>
-            <h5>
-                Email: amanda_ma98@berkeley.edu
-            </h5>
+            <p id="specialty">
+            specializes in <b><i>data engineering, machine learning & analytics</i></b>
+            </p>
+            <p id="job-title">
+            behavioral integrity <b>data engineer</b> at <b>Meta</b> from 2022-23
+            </p>
+            <p id="school"> 
+            <b>UC Berkeley BA Data Science '21</b>
+            </p>
             <a href="https://www.linkedin.com/in/mandiema/" target="_blank">LinkedIn </a>
             <a href="https://drive.google.com/file/d/1iXgByv0DVqTEMb96nAEs0XgvF4reslfm/view?usp=sharing" target="_blank">Resume </a>
             <a href="https://github.com/mandiema" target="_blank">Github</a>
+            <h6>
+                <img src={email} width="4%"/> amanda_ma98@berkeley.edu 
+            </h6>
             <p>
-                Hello! My name is Amanda, and I was born & raised in San Francisco. <br/>
                 <b>I am currently working on developing this site and adding
                 my projects - check back later! :)</b>
             </p>
+            <section id="scroll-arrow">
+                <img src={arrow} width="3%"></img>
+                <img src={arrow} width="3%"></img>
+                <img src={arrow} width="3%"></img>
+            </section>
       </div>
     )
 }
