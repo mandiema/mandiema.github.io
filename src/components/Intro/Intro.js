b3e0a619af34e6f6a31bc5c0b8738618dde251c0
Amanda Ma <amanda_ma98@berkeley.edu>
2023-12-15 19:50:41 -0800

    intro section code reorg.

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -10,29 +10,32 @@ export default function Intro() {
             <h1 id="name">Amanda Ma</h1>
             <img src={headshot} id="headshot" alt="logo" />
             <p id="specialty">
-            specializes in <b><i>data engineering, machine learning & analytics</i></b>
+                specializes in <b><i>data engineering, machine learning & analytics</i></b>
             </p>
             <p id="job-title">
-            behavioral integrity <b>data engineer</b> at <b>Meta</b> from 2022-23
+                behavioral integrity <b>data engineer</b> at <b>Meta</b> from 2022-23
             </p>
             <p id="school"> 
-            <b>UC Berkeley BA Data Science '21</b>
+                <b>UC Berkeley BA Data Science '21</b>
             </p>
-            <a href="https://www.linkedin.com/in/mandiema/" target="_blank">LinkedIn </a>
-            <a href="https://drive.google.com/file/d/1iXgByv0DVqTEMb96nAEs0XgvF4reslfm/view?usp=sharing" target="_blank">Resume </a>
-            <a href="https://github.com/mandiema" target="_blank">Github</a>
-            <h6>
-                <img src={email} width="4%"/> amanda_ma98@berkeley.edu 
-            </h6>
+            <div id="links">
+                <a href="https://www.linkedin.com/in/mandiema/" target="_blank">LinkedIn</a>
+                <a href="https://drive.google.com/file/d/1iXgByv0DVqTEMb96nAEs0XgvF4reslfm/view?usp=sharing" target="_blank">Resume</a>
+                <a href="https://github.com/mandiema" target="_blank">Github</a>
+            </div>
+            <div id="email">
+                <img src={email} width="4%"/>
+                amanda_ma98@berkeley.edu 
+            </div>
             <p id="note">
                 <b>I am currently working on developing this site and adding
                 my projects - check back later! :)</b>
             </p>
-            <section id="scroll-arrow">
+            <div id="scroll-arrow">
                 <img src={arrow} width="3%"></img>
                 <img src={arrow} width="3%"></img>
                 <img src={arrow} width="3%"></img>
-            </section>
+            </div>
       </div>
     )
 }
