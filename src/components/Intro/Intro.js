5f9fbb7a3ab6f6e9a8e9e0a981215f8056a83706
Amanda Ma <amanda_ma98@berkeley.edu>
2024-04-11 17:51:40 -0700

    modify intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -10,7 +10,7 @@ export default function Intro() {
             <h1 id="name">Amanda Ma</h1>
             <img src={headshot} id="headshot" alt="logo" />
             <p id="specialty">
-                specializes in <b><i>data engineering, machine learning & analytics</i></b>
+            Results‐driven data professional with 1.5 years of experience using big data technologies to deliver impactful outcomes in product analytics.
             </p>
             <p id="job-title">
                 behavioral integrity <b>data engineer</b> at <b>Meta</b> from 2022-23
@@ -20,17 +20,15 @@ export default function Intro() {
             </p>
             <div id="links">
                 <a href="https://www.linkedin.com/in/mandiema/" target="_blank">LinkedIn</a>
-                <a href="https://drive.google.com/file/d/1gtgCCvZ4kcMqpC9WkhPSJfCdjpXFeyHu/view?usp=sharing" target="_blank">Resume</a>
+                <a href="https://drive.google.com/file/d/1jEhFrLgoQ2Lx88murPSkJoNqpKTT5_fp/view?usp=sharing" target="_blank">Resume</a>
                 <a href="https://github.com/mandiema" target="_blank">Github</a>
+                <a href="https://medium.com/@amanda_ma98" target="_blank">Medium</a>
+                <a href="https://adplist.org/members/amanda-ma" target="_blank">ADPList</a>
             </div>
             <div id="email">
                 <img src={email} width="4%"/>
                 amanda_ma98@berkeley.edu 
             </div>
-            <p id="note">
-                <b>I am currently working on developing this site and adding
-                my projects - check back later! :)</b>
-            </p>
             <div id="scroll-arrow">
                 <img src={arrow} width="3%"></img>
                 <img src={arrow} width="3%"></img>
